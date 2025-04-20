38eb222ec69972f8c0be16ea1e6d9a86fc3517d0
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2025-04-20 17:08:19 -0400

    remove unused template residue code

--- src/exports/rsc.ts
+++ src/exports/rsc.ts
@@ -1,3 +1,2 @@
-export { BeforeDashboardServer } from "../components/BeforeDashboardServer.js";
 export { DocumentIDCell } from "../components/DocumentIDCell.js";
 export { PreviousVersionIDCell } from "../components/PreviousVersionIDCell.js";
