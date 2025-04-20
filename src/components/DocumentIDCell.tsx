d4f7693f28b35a42ca3db000deaba5b09c73bb1f
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2025-04-20 16:15:01 -0400

    fix: add guard for missing/falsy cellData

--- src/components/DocumentIDCell.tsx
+++ src/components/DocumentIDCell.tsx
@@ -2,6 +2,9 @@ import type { DefaultServerCellComponentProps } from "payload";
 
 export const DocumentIDCell = (props: DefaultServerCellComponentProps) => {
   const { cellData, rowData } = props;
+  if (!cellData) {
+    return "Not Found";
+  }
 
   return <a href={`/admin/collections/${rowData["resourceType"]}/${cellData}`}>{cellData}</a>;
 };
