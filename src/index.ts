3dab9af808330ad1f29bfcd1992df48357ccb8da
Atlas Gong <68199735+atlasgong@users.noreply.github.com>
2025-04-20 16:54:31 -0400

    wrap payload local api requests in try-catch block

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import type { Config, Operation, PayloadRequest } from "payload";
+import type { Config, JsonObject, Operation, PayloadRequest, TypeWithID, TypeWithVersion } from "payload";
 
 import type { PayloadSentinelConfig } from "./config.js";
 
@@ -31,28 +31,42 @@ export const payloadSentinel =
       if (options.disabled || !options.operations[operation] || !req.user?.id) {
         return;
       }
-      const latestVersions = await req.payload.findVersions({
-        collection: collectionSlug,
-        limit: 1,
-        sort: "-updatedAt",
-        where: {
-          parent: {
-            equals: doc.id,
+      let previousVersion: TypeWithVersion<JsonObject & TypeWithID> | undefined;
+      try {
+        const latestVersions = await req.payload.findVersions({
+          collection: collectionSlug,
+          limit: 1,
+          sort: "-updatedAt",
+          where: {
+            parent: {
+              equals: doc.id,
+            },
           },
-        },
-      });
-      const previousVersion = latestVersions.totalDocs > 0 ? latestVersions.docs[0] : undefined;
-      await req.payload.create({
-        collection: options.auditLogsCollection,
-        data: {
-          documentId: doc.id,
-          operation,
-          previousVersionId: previousVersion?.id,
-          resourceType: collectionSlug,
-          timestamp: new Date(),
-          user: req.user?.id,
-        },
-      });
+        });
+        previousVersion = latestVersions.totalDocs > 0 ? latestVersions.docs[0] : undefined;
+      } catch (error) {
+        const message = error instanceof Error ? error.message : String(error);
+        throw new Error(
+          "System error: unable to retrieve versions. This is not because versioning is disabled or missing for this document. Details: " +
+            message,
+        );
+      }
+      try {
+        await req.payload.create({
+          collection: options.auditLogsCollection,
+          data: {
+            documentId: doc.id,
+            operation,
+            previousVersionId: previousVersion?.id,
+            resourceType: collectionSlug,
+            timestamp: new Date(),
+            user: req.user?.id,
+          },
+        });
+      } catch (error) {
+        const message = error instanceof Error ? error.message : String(error);
+        throw new Error("Error creating audit log: " + message);
+      }
     }
 
     async function logGlobalAudit({
@@ -67,23 +81,37 @@ export const payloadSentinel =
       if (options.disabled || !options.operations[operation] || !req.user?.id) {
         return;
       }
-      const latestVersions = await req.payload.findGlobalVersions({
-        slug: globalSlug,
-        limit: 1,
-        sort: "-updatedAt",
-      });
-      const previousVersion = latestVersions.totalDocs > 0 ? latestVersions.docs[0] : undefined;
-      await req.payload.create({
-        collection: options.auditLogsCollection,
-        data: {
-          documentId: globalSlug,
-          operation,
-          previousVersionId: previousVersion?.id,
-          resourceType: globalSlug,
-          timestamp: new Date(),
-          user: req.user?.id,
-        },
-      });
+      let previousVersion: TypeWithVersion<JsonObject> | undefined;
+      try {
+        const latestVersions = await req.payload.findGlobalVersions({
+          slug: globalSlug,
+          limit: 1,
+          sort: "-updatedAt",
+        });
+        previousVersion = latestVersions.totalDocs > 0 ? latestVersions.docs[0] : undefined;
+      } catch (error) {
+        const message = error instanceof Error ? error.message : String(error);
+        throw new Error(
+          "System error: unable to retrieve versions. This is not because versioning is disabled or missing for this document. Details: " +
+            message,
+        );
+      }
+      try {
+        await req.payload.create({
+          collection: options.auditLogsCollection,
+          data: {
+            documentId: globalSlug,
+            operation,
+            previousVersionId: previousVersion?.id,
+            resourceType: globalSlug,
+            timestamp: new Date(),
+            user: req.user?.id,
+          },
+        });
+      } catch (error) {
+        const message = error instanceof Error ? error.message : String(error);
+        throw new Error("Error creating audit log: " + message);
+      }
     }
 
     if (!config.collections) {
