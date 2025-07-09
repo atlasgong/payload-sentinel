efd8595038dadc07b3d437791bfdc7807fe8c4b7
Sam Wheeler <104921112+swheeler7@users.noreply.github.com>
2025-07-09 11:58:06 -0700

    fix typo
    Co-authored-by: ellipsis-dev[bot] <65095814+ellipsis-dev[bot]@users.noreply.github.com>

--- src/defaults.ts
+++ src/defaults.ts
@@ -12,7 +12,7 @@ export const defaultConfig: Required<PayloadSentinelConfig> = {
     return Boolean(user);
   },
   auditLogCollection: "audit-log",
-  auditLogCollectionGroup: "Payload Sentinal",
+  auditLogCollectionGroup: "Payload Sentinel",
   authCollection: "users",
   dateFormat: "EEE, dd MMM yyyy HH:mm:ss",
   disabled: false,
